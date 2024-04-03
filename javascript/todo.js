fd585bdb6e225d8b5a5b3457c9f1c9b7232bfe85
kinyuap <kinyuapitah5@gmail.com>
2024-04-03 07:04:09 +0300

    create and delete

--- javascript/todo.js
+++ javascript/todo.js
@@ -1,12 +1,27 @@
+
+
+let todoIsHtml = '';
+
 function todoList() {
-  let todoIsHtml = '';
+  
   const inputValue = document.getElementById('todoInput').value;
    document.getElementById('todoInput').value = '';
-  const text = document.createTextNode(inputValue);
-  const newInputValue = document.createElement('li');
-    newInputValue.appendChild(text);
-  const html = document.getElementById('todoList').appendChild(newInputValue)
+  const inputDate = document.getElementById('todoDate').value;
+    document.getElementById('todoDate').value = '';
+  // const text = document.createTextNode(inputValue);
+  // const newInputValue = document.createElement('li');
+  //   newInputValue.appendChild(text);
+  // const html = document.getElementById('todoList').appendChild(newInputValue)
+  const html = `
+  <div>${inputValue}</div>
+  <div>${inputDate}</div>
+  `;
+ todoIsHtml += html;
+
+  let newInputValue = document.getElementById('inputDisplay')
+ newInputValue.innerHTML = todoIsHtml;
+
 };
 document.querySelector('.button').addEventListener('click', () => {
- todoList();
+ todoList()
 });
