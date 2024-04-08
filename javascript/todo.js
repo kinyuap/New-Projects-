b63fa568f3bf1367e48e90d1e2bfc7984149f5da
kinyuap <kinyuapitah5@gmail.com>
2024-04-08 07:05:09 +0300

    Updatting todo list

--- javascript/todo.js
+++ javascript/todo.js
@@ -4,7 +4,7 @@
 
 function todoList() {
    let todoIsHtml = '';
-   let todoList = [];
+  
   const inputValue = document.getElementById('todoInput').value;
    document.getElementById('todoInput').value = '';
   const inputDate = document.getElementById('todoDate').value;
@@ -13,25 +13,20 @@ function todoList() {
   // const newInputValue = document.createElement('li');
   //   newInputValue.appendChild(text);
   // const html = document.getElementById('todoList').appendChild(newInputValue)
+
   const html = `
   <div>${inputValue}</div>
   <div>${inputDate}</div>
-  <button id="js-delete-btn">Delete</button>
-  `;
- todoIsHtml += html;
- todoList = todoIsHtml;
- console.log(todoList);
-  let newInputValue = document.getElementById('inputDisplay').innerHTML = todoIsHtml;
-};
+  <button class="js-delete-btn">Delete</button>
+  `
 
-function deletebtn()
-{
- 
-};
+  let newInputValue = document.getElementById('inputDisplay');
+  newInputValue.innerHTML += html;
 
-document.querySelector('.button').addEventListener('click', () => {
- todoList()
-});
-// document.getElementById('js-delete-btn').addEventListener('click', () => {
-  
-// })
+ let deleteButton =  newInputValue.querySelector('.js-delete-btn');
+ deleteButton.addEventListener('click', () => {
+  deleteButton.parentElement.remove();
+ });
+}
+
+document.querySelector('.button').addEventListener('click', todoList);
