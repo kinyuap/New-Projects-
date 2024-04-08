695fe76d511722c3e0534cd266444352de0ea8db
kinyuap <kinyuapitah5@gmail.com>
2024-04-08 12:46:16 +0300

    Updating todo Lists

--- javascript/todo.js
+++ javascript/todo.js
@@ -2,31 +2,65 @@
 
 
 
-function todoList() {
-   let todoIsHtml = '';
+// function todoList() {
+//    let todoIsHtml = '';
   
-  const inputValue = document.getElementById('todoInput').value;
-   document.getElementById('todoInput').value = '';
-  const inputDate = document.getElementById('todoDate').value;
-    document.getElementById('todoDate').value = '';
-  // const text = document.createTextNode(inputValue);
-  // const newInputValue = document.createElement('li');
-  //   newInputValue.appendChild(text);
-  // const html = document.getElementById('todoList').appendChild(newInputValue)
+//   const inputValue = document.getElementById('todoInput').value;
+//    document.getElementById('todoInput').value = '';
+//   const inputDate = document.getElementById('todoDate').value;
+//     document.getElementById('todoDate').value = '';
+//   // const text = document.createTextNode(inputValue);
+//   // const newInputValue = document.createElement('li');
+//   //   newInputValue.appendChild(text);
+//   // const html = document.getElementById('todoList').appendChild(newInputValue)
 
-  const html = `
-  <div>${inputValue}</div>
-  <div>${inputDate}</div>
-  <button class="js-delete-btn">Delete</button>
-  `
+//   const html = `
+//   <div>${inputValue}</div>
+//   <div>${inputDate}</div>
+//   <button class="js-delete-btn">Delete</button>
+//   `
 
-  let newInputValue = document.getElementById('inputDisplay');
-  newInputValue.innerHTML += html;
+//   let newInputValue = document.getElementById('inputDisplay');
+//   newInputValue.innerHTML += html;
 
- let deleteButton =  newInputValue.querySelector('.js-delete-btn');
- deleteButton.addEventListener('click', () => {
-  deleteButton.parentElement.remove();
- });
+//  let deleteButtons = newInputValue.querySelectorAll('.js-delete-btn');
+// deleteButtons.forEach(deleteButton => {
+//  deleteButton.addEventListener('click', (event) => {
+//   event.target.parentElement.remove();
+//  });
+// });
+// }
+
+// document.querySelector('.button').addEventListener('click', () => {
+//   todoList()
+// });
+function todoList() {
+  let todoIsHtml = '';
+ 
+ const inputValue = document.getElementById('todoInput').value;
+  document.getElementById('todoInput').value = '';
+ const inputDate = document.getElementById('todoDate').value;
+   document.getElementById('todoDate').value = '';
+
+ const html = `
+ <div>${inputValue}</div>
+ <div>${inputDate}</div>
+ <button class="js-delete-btn">Delete</button>
+ `
+
+ let newInputValue = document.getElementById('inputDisplay');
+ newInputValue.innerHTML += html;
+
+let deleteButtons = newInputValue.querySelectorAll('.js-delete-btn');
+deleteButtons.forEach(deleteButton => {
+deleteButton.addEventListener('click', (event) => {
+ event.target.parentElement.remove();
+
+});
+
+});
 }
 
-document.querySelector('.button').addEventListener('click', todoList);
+document.querySelector('.button').addEventListener('click', () => {
+ todoList()
+});
